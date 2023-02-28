0bcc8f5b03e9c8362b4d7a98b808de9da05bd7b6
mgdammy14 <mgdammy14@gmail.com>
2023-02-28 14:08:08 -0500

    create meeting v2

--- src/scenes/meeting/meetingForm.jsx
+++ src/scenes/meeting/meetingForm.jsx
@@ -1,4 +1,4 @@
-import { Box, Button, Typography } from '@mui/material';
+import { Box, Button, Typography, useTheme } from '@mui/material';
 import { Formik } from 'formik';
 import React, { useEffect, useState } from 'react';
 import { useLocation } from "react-router-dom";
@@ -15,12 +15,17 @@ import MenuItem from '@mui/material/MenuItem';
 import { enviroments } from '../../env';
 import axios from 'axios';
 import Alerts from '../../components/Alert';
+import { tokens } from '../../themes';
+import { DataGrid } from "@mui/x-data-grid";
+import Checkbox from '@mui/material/Checkbox';
 
 const MeetingForm = () => {
 
     const { state } = useLocation();
 
-    console.log(state);
+
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode);
 
     const [names, setNames] = useState(state.item.clientName + ' ' + state.item.clientLastname);
     const [date, setDate] = React.useState(dayjs());
@@ -30,6 +35,7 @@ const MeetingForm = () => {
     const [statusMeetingData, setStatusMeetingData] = useState([]);
     const [collaborators, setCollaborators] = useState([]);
     const [paymentMethods, setPaymentMethods] = useState([]);
+    const [services, setServices] = useState([]);
 
     const initialValues = {
         id: ""
@@ -51,6 +57,12 @@ const MeetingForm = () => {
         const req = await fetch(enviroments.urlBackend + "paymentMethod")
         const json = await req.json()
         setPaymentMethods(json.objModel);
+    }
+
+    async function GetServicesDataAPI() {
+        const req = await fetch(enviroments.urlBackend + "service")
+        const json = await req.json()
+        setServices(json.objModel);
     }
 
     const handleChangeCollaborator = (event) => {
@@ -102,7 +114,54 @@ const MeetingForm = () => {
         GetStatusMeetingDataAPI();
         GetCollaboratorsDataAPI();
         GetPaymentMethodDataAPI();
+        GetServicesDataAPI();
     }, [])
+
+    const columns = [
+        {
+            field: "id",
+            headerName: "ID Servicio"
+        },
+        {
+            field: "serviceName",
+            headerName: "Servicio",
+            flex: 1
+        },
+        {
+            field: "servicePrice",
+            headerName: "Precio",
+            flex: 1,
+            renderCell: ({ row: { servicePrice } }) => {
+                return (
+                    <Typography>
+                        S/ {servicePrice}
+                    </Typography>
+                )
+            }
+        },
+        {
+            field: "serviceEstimatedTime",
+            headerName: "Tiempo",
+            flex: 1,
+            renderCell: ({ row: { serviceEstimatedTime } }) => {
+                return (
+                    <Typography>
+                        {serviceEstimatedTime} minutos
+                    </Typography>
+                )
+            }
+        },
+        {
+            field: "select",
+            headerName: "Seleccionar",
+            flex: 1,
+            renderCell: ({ row: { serviceEstimatedTime } }) => {
+                return (
+                    <Checkbox />
+                )
+            }
+        }
+    ]
 
     return (
         <Box m="20px">
@@ -150,7 +209,6 @@ const MeetingForm = () => {
                                     />
                                 </Box>
                             </Box>
-
                             <Box
                                 m="10px 0 0 0"
                                 display="grid"
@@ -209,6 +267,41 @@ const MeetingForm = () => {
                                         }}
                                     />
                                 </LocalizationProvider>
+                            </Box>
+                            <Box
+                                m="10px 0 0 0"
+                                display="grid"
+                                gap="30px"
+                                gridTemplateColumns="repeat(4, minmax(0, 1fr))"
+                            >
+                                <Box
+                                    height="30vh"
+                                    sx={{
+                                        gridColumn: "span 2",
+                                        "& .MuiDataGrid-root": {
+                                            border: "none",
+                                        },
+                                        "& .MuiDataGrid-cell": {
+                                            borderBottom: "none",
+                                        },
+                                        "& .name-column--cell": {
+                                            color: colors.greenAccent[300]
+                                        },
+                                        "& .MuiDataGrid-columnHeaders": {
+                                            backgroundColor: colors.blueAccent[700],
+                                            borderBottom: "none"
+                                        },
+                                        "& .MuiDataGrid-virtualScroller": {
+                                            backgroundColo: colors.primary[400]
+                                        },
+                                        "& .MuiDataGrid-footerContainer": {
+                                            borderTop: "none",
+                                            backgroundColor: colors.blueAccent[700]
+                                        }
+                                    }}
+                                >
+                                    <DataGrid rows={services} columns={columns} />
+                                </Box>
                             </Box>
                         </Box>
                         <Box display="flex" justifyContent="end" mt="30px">
