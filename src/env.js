758cb51e4a28b0c696a6d66aca4ded933636e065
cizquierdo-u <cizquierdo@unify-tec.com>
2025-01-21 00:31:48 -0500

    commit sin node

--- src/env.js
+++ src/env.js
@@ -1,4 +1,4 @@
 export const enviroments = {
-  urlBackend: "https://backendgailgir.azurewebsites.net/api/",
-  // urlBackend: "https://localhost:5001/api/",
-};
+  //urlBackend: "https://backendgailgir.azurewebsites.net/api/",
+  urlBackend: "https://gailgirlapi.somee.com/api/",
+}; 
